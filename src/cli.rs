4d8e8010e8d7b2bb3a3527ad0389e5d859bf9c51
Toshiki <bushy.trivial.0o@icloud.com>
2024-12-10 21:37:45 +0900

    Enhance CLI functionality in `src/cli.rs` by adding shell completion support
    - Introduced a new argument `generate_completions` to the `CliArgs` struct for generating shell completions.
    - Refactored the routing logic to handle shell completion requests separately from logging.
    - Added a new function `generate_completions` to facilitate the generation of command-line completions for various shells.
    - Improved the overall structure and readability of the CLI argument handling code.

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,10 @@
-use std::env;
-
 use crate::youtube::interface::cli::YoutubeArgs;
-use clap::{Parser, Subcommand};
+use clap::{CommandFactory, Parser, Subcommand};
+use clap_complete::{generate, Generator, Shell};
 use enum_dispatch::enum_dispatch;
 use env_logger;
 use log::Level;
+use std::{env, io::stdout};
 
 #[enum_dispatch]
 pub trait Route {
@@ -17,8 +17,11 @@ pub struct CliArgs {
     #[arg(short, long, global = true, help = "Show logs")]
     verbose: bool,
 
+    #[arg(long, value_name = "SHELL_NAME", help = "Generate shell completions")]
+    generate_completions: Option<Shell>,
+
     #[command(subcommand)]
-    command: CliSubcommand,
+    command: Option<CliSubcommand>,
 }
 
 #[derive(Subcommand, Debug)]
@@ -29,11 +32,27 @@ enum CliSubcommand {
 
 impl Route for CliArgs {
     fn route(&self) {
-        if self.verbose {
-            env::set_var("RUST_LOG", Level::Trace.to_string());
-        }
-        env_logger::init();
+        if let Some(shell) = &self.generate_completions {
+            generate_completions(shell.clone());
+        } else {
+            initialize_logger(self.verbose);
 
-        self.command.route();
+            if let Some(command) = &self.command {
+                command.route();
+            }
+        }
     }
+}
+
+fn generate_completions<G: Generator>(gen: G) {
+    let mut cmd = CliArgs::command();
+    let bin_name = cmd.get_name().to_string();
+    generate(gen, &mut cmd, bin_name, &mut stdout());
+}
+
+fn initialize_logger(verbose: bool) {
+    if verbose {
+        env::set_var("RUST_LOG", Level::Trace.to_string());
+    }
+    env_logger::init();
 }

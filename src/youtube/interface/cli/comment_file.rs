6fc6ebc40d3e05340ca35c1af1d07bf5ea470517
Toshiki <bushy.trivial.0o@icloud.com>
2024-12-14 22:13:20 +0900

    Refactor comment_file CLI arguments to improve structure and usability
    - Updated the `Args` struct in `comment_file.rs` to replace the `value` field with `input_file` and an optional `output_file`, enhancing clarity in command-line argument handling.
    - Added value hints for file paths to improve user experience when specifying input and output files.
    - Removed unused code related to the previous `value` field, streamlining the implementation.

--- src/youtube/interface/cli/comment_file.rs
+++ src/youtube/interface/cli/comment_file.rs
@@ -1,14 +1,21 @@
+use clap::ValueHint;
+use std::path::PathBuf;
+
 use crate::cli::Route;
 
 #[derive(clap::Args, Debug)]
 #[command(name = "Comment File Feature")]
 pub(super) struct Args {
-    value: String,
+    #[clap(value_name = "INPUT FILE", value_hint = ValueHint::FilePath)]
+    input_file: PathBuf,
+
+    #[clap(short, long, value_name = "OUTPUT FILE", value_hint = ValueHint::FilePath)]
+    output_file: Option<PathBuf>,
 }
 
 impl Route for Args {
     fn route(&self) {
-        let value = self.value.clone();
-        println!("{}", value);
+        // let value = self.value.clone();
+        // println!("{}", value);
     }
 }

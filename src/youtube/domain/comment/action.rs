f71f99358b02a5c67aadc2d47ce1a2a196231ae1
Toshiki <bushy.trivial.0o@icloud.com>
2024-12-15 14:19:22 +0900

    Reorder `use` statements in action.rs for improved clarity
    - Moved the `serde` imports below the `item` import to follow best practices for module organization.
    - This change enhances the readability and structure of the `action.rs` file, aligning with the conventions outlined in the new documentation on module and use order.

--- src/youtube/domain/comment/action.rs
+++ src/youtube/domain/comment/action.rs
@@ -1,7 +1,7 @@
 mod item;
 
-use serde::{Deserialize, Serialize};
 use item::Item;
+use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]

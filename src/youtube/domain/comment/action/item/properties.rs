54327050e5c05ed6fe9e390f549867fc5b6938f0
Toshiki <bushy.trivial.0o@icloud.com>
2024-12-22 18:51:36 +0900

    Refactor comment action module and its dependencies

--- src/youtube/domain/comment/action/item/properties.rs
+++
@@ -1,7 +0,0 @@
-pub mod accessibility;
-pub mod author_badge;
-pub mod author_name;
-pub mod context_menu_endpoint;
-pub mod icon;
-pub mod message;
-pub mod thumbnail;
--- src/youtube/domain/comment/action/item/properties/author_name.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct AuthorName {
-    pub simple_text: String,
-}

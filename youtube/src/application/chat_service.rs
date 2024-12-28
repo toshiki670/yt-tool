25766e0303d4e6435c4cccb544eb98fdae22c6bc
Toshiki <bushy.trivial.0o@icloud.com>
2024-12-28 15:38:57 +0900

    Refactor chat service and remove comment service

---
+++ youtube/src/application/chat_service.rs
@@ -0,0 +1,20 @@
+use crate::domain::repositories::ChatRepository;
+
+
+
+pub struct ChatConvertService {
+    from_chat: Box<dyn ChatRepository>,
+    to_chat: Box<dyn ChatRepository>,
+}
+
+
+impl ChatConvertService {
+    pub fn new(from_chat: Box<dyn ChatRepository>, to_chat: Box<dyn ChatRepository>) -> Self {
+        Self { from_chat, to_chat }
+    }
+
+    pub fn convert(&self) -> anyhow::Result<()> {
+        let chats = self.from_chat.all()?;
+        self.to_chat.bulk_create(chats)
+    }
+}

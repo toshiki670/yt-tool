f0b3c63f6b2edfaccda343615d5d1447ba07f513
Toshiki <bushy.trivial.0o@icloud.com>
2024-12-28 15:39:06 +0900

    Refactor ChatRepository trait in repositories.rs for error handling

--- youtube/src/domain/repositories.rs
+++ youtube/src/domain/repositories.rs
@@ -1,14 +1,6 @@
 use super::chat::Chat;
 
-
-
-// pub trait Repositories {
-//     fn chat_repository(&self) -> Box<dyn ChatRepository>;
-// }
-
 pub trait ChatRepository {
-    fn all(&self) -> Vec<Chat>;
-    fn bulk_create(&self, chats: Vec<Chat>);
+    fn all(&self) -> anyhow::Result<Vec<Chat>>;
+    fn bulk_create(&self, chats: Vec<Chat>) -> anyhow::Result<()>;
 }
-
-

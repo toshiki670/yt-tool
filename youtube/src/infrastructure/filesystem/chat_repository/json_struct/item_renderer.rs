8b821104ace37d181cbc18a9609f5d240787028a
Toshiki <bushy.trivial.0o@icloud.com>
2024-12-30 18:32:55 +0900

    Refactor item_renderer.rs to use chrono for timestamp handling

--- youtube/src/infrastructure/filesystem/chat_repository/json_struct/item_renderer.rs
+++ youtube/src/infrastructure/filesystem/chat_repository/json_struct/item_renderer.rs
@@ -3,6 +3,7 @@ mod live_chat_renderer;
 mod live_chat_ticker_paid_message_item_renderer;
 mod values;
 
+use chrono::prelude::*;
 use live_chat_paid_message_renderer::LiveChatPaidMessageRenderer;
 use live_chat_renderer::LiveChatRenderer;
 use live_chat_ticker_paid_message_item_renderer::LiveChatTickerPaidMessageItemRenderer;
@@ -22,7 +23,7 @@ pub enum Item {
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CommonRenderer {
-    pub timestamp_usec: String,
+    pub timestamp_usec: DateTime<Utc>,
     pub author_external_channel_id: String,
     pub author_name: String,
     pub message: String,

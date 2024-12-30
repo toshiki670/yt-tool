3d70bcc2386b8377054c917c6c09a09eb1995e28
Toshiki <bushy.trivial.0o@icloud.com>
2024-12-31 01:28:29 +0900

    Refactor ContextMenuEndpoint struct to use IgnoreNavigation in web_command_metadata field

--- youtube/src/infrastructure/filesystem/chat_repository/json_struct/item_renderer/values/context_menu_endpoint.rs
+++ youtube/src/infrastructure/filesystem/chat_repository/json_struct/item_renderer/values/context_menu_endpoint.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-use super::web_command_metadata::WebCommandMetadata;
+use super::{ignore_navigation::IgnoreNavigation, web_command_metadata::WebCommandMetadata};
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ContextMenuEndpoint {
     pub click_tracking_params: Option<String>,
-    pub command_metadata: WebCommandMetadata,
+    pub command_metadata: WebCommandMetadata<IgnoreNavigation>,
     pub live_chat_item_context_menu_endpoint: Params,
 }
 

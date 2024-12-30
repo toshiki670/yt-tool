741b1e22ddab0a19d6b415d5b1d3b60ffc2dfd40
Toshiki <bushy.trivial.0o@icloud.com>
2024-12-30 23:01:51 +0900

    Refactor module structure to use ChatEntity in csv_struct.rs

--- youtube/src/infrastructure/filesystem/chat_repository/csv_struct.rs
+++ youtube/src/infrastructure/filesystem/chat_repository/csv_struct.rs
@@ -1,19 +1,6 @@
-use crate::domain::chat::{Category, Chat as ChatDomain};
+use crate::domain::chat::ChatEntity;
 use serde::{Deserialize, Serialize};
-use std::{fmt, fs::File};
-
-impl fmt::Display for Category {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Category::ChatTextMessage => write!(f, "Chat text message"),
-            Category::ChatPaidMessage => write!(f, "Chat paid message"),
-            Category::ChatSponsorshipsGiftRedemptionAnnouncement => {
-                write!(f, "Chat sponsorships gift redemption announcement")
-            }
-            Category::ChatTickerPaidMessageItem => write!(f, "Chat ticker paid message item"),
-        }
-    }
-}
+use std::fs::File;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct CsvChat {
@@ -27,7 +14,7 @@ pub struct CsvChat {
 }
 
 impl CsvChat {
-    pub fn bulk_create_file(chats: Vec<ChatDomain>, file: &File) -> anyhow::Result<()> {
+    pub fn bulk_create_file(chats: Vec<ChatEntity>, file: &File) -> anyhow::Result<()> {
         let mut wtr = csv::Writer::from_writer(file);
 
         for chat in chats {
@@ -40,10 +27,10 @@ impl CsvChat {
     }
 }
 
-impl<'a> From<ChatDomain> for CsvChat {
-    fn from(chat: ChatDomain) -> Self {
+impl<'a> From<ChatEntity> for CsvChat {
+    fn from(chat: ChatEntity) -> Self {
         Self {
-            timestamp_usec: chat.timestamp_usec,
+            timestamp_usec: chat.posted_at.to_string(),
             author_external_channel_id: chat.author_external_channel_id,
             author_name: chat.author_name,
             message: chat.message,

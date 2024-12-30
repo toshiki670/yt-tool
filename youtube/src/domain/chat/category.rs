5414f7f808b46d449297d5163b68829836d25b98
Toshiki <bushy.trivial.0o@icloud.com>
2024-12-30 23:01:40 +0900

    Refactor CategoryValue enum to implement fmt::Display trait

--- youtube/src/domain/chat/category.rs
+++ youtube/src/domain/chat/category.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Default, Debug, Clone, PartialEq)]
 pub(crate) enum CategoryValue {
     #[default]
@@ -5,4 +7,17 @@ pub(crate) enum CategoryValue {
     ChatPaidMessage,
     ChatSponsorshipsGiftRedemptionAnnouncement,
     ChatTickerPaidMessageItem,
+}
+
+impl fmt::Display for CategoryValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            CategoryValue::ChatTextMessage => write!(f, "Chat text message"),
+            CategoryValue::ChatPaidMessage => write!(f, "Chat paid message"),
+            CategoryValue::ChatSponsorshipsGiftRedemptionAnnouncement => {
+                write!(f, "Chat sponsorships gift redemption announcement")
+            }
+            CategoryValue::ChatTickerPaidMessageItem => write!(f, "Chat ticker paid message item"),
+        }
+    }
 }

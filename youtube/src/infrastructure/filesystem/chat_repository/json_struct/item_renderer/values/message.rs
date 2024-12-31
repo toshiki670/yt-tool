18ff6267cc2a836217b87b5a93a17ae0c84786fb
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-01 00:34:28 +0900

    Refactor Message and Run structs in JSON representation to support optional text and emojis, enhancing serialization capabilities

--- youtube/src/infrastructure/filesystem/chat_repository/json_struct/item_renderer/values/message.rs
+++ youtube/src/infrastructure/filesystem/chat_repository/json_struct/item_renderer/values/message.rs
@@ -1,21 +1,46 @@
 use serde::{Deserialize, Serialize};
 
+use super::thumbnails::Thumbnails;
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Message {
-    pub runs: Vec<Text>,
+    pub runs: Vec<Run>,
+}
+
+impl Into<String> for Message {
+    fn into(self) -> String {
+        self.runs.into_iter().map(|run| run.into()).collect::<Vec<String>>().join("")
+    }
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Text {
+pub struct Run {
     pub bold: Option<bool>,
     pub italics: Option<bool>,
-    pub text: String,
+    pub text: Option<String>,
+    pub emoji: Option<Emoji>,
 }
 
-impl Into <String> for Message {
+impl Into<String> for Run {
     fn into(self) -> String {
-        self.runs.iter().map(|run| run.text.clone()).collect()
+        self.text.unwrap_or_else(|| self.emoji.unwrap_or_default().into())
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Emoji {
+    pub emoji_id: String,
+    pub shortcuts: Vec<String>,
+    pub search_terms: Vec<String>,
+    pub image: Thumbnails,
+    pub is_custom_emoji: bool,
+}
+
+impl Into<String> for Emoji {
+    fn into(self) -> String {
+        self.shortcuts.join(", ")
     }
 }

cd879c4288343deaeee313eb3d9feb0f9e1f9285
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-03 02:01:13 +0900

    Refactor ChatConvertService to improve clarity and maintainability
    - Updated the ChatConvertService struct to rename fields from `from_chat` and `to_chat` to `live_chat` and `simple_chat`, respectively, enhancing the clarity of their purpose.
    - Adjusted method implementations to reflect the new field names, ensuring consistency and improving code readability.
    - These changes contribute to a more organized and maintainable chat service structure, aligning with ongoing efforts to enhance modularity in the codebase.

--- youtube/src/application/chat_service.rs
+++ youtube/src/application/chat_service.rs
@@ -6,20 +6,20 @@ use anyhow::Context;
 use std::convert::TryInto;
 
 pub struct ChatConvertService {
-    from_chat: Box<dyn FetchLiveChatRepository>,
-    to_chat: Box<dyn SaveSimpleChatRepository>,
+    live_chat: Box<dyn FetchLiveChatRepository>,
+    simple_chat: Box<dyn SaveSimpleChatRepository>,
 }
 
 impl ChatConvertService {
     pub fn new(
-        from_chat: Box<dyn FetchLiveChatRepository>,
-        to_chat: Box<dyn SaveSimpleChatRepository>,
+        live_chat: Box<dyn FetchLiveChatRepository>,
+        simple_chat: Box<dyn SaveSimpleChatRepository>,
     ) -> Self {
-        Self { from_chat, to_chat }
+        Self { live_chat, simple_chat }
     }
 
     pub fn convert(&self) -> anyhow::Result<()> {
-        let live_chats = self.from_chat.all()?;
+        let live_chats = self.live_chat.all()?;
         let mut simple_chats = Vec::new();
 
         for (line_number, live_chat) in live_chats.into_iter().enumerate() {
@@ -29,6 +29,6 @@ impl ChatConvertService {
             simple_chats.extend(chats);
         }
 
-        self.to_chat.bulk_create(simple_chats)
+        self.simple_chat.bulk_create(simple_chats)
     }
 }

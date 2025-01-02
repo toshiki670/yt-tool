430c774069c8915776374e09d3f4c9c1f4237f5c
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-02 20:49:15 +0900

    Refactor ChatConvertService to utilize new repository interfaces
    - Updated the ChatConvertService struct to replace the previous LiveChatRepository and SimpleChatRepository with FetchLiveChatRepository and SaveSimpleChatRepository, enhancing modularity and clarity in the chat service.
    - This change aligns with ongoing efforts to improve the organization and maintainability of the chat domain, paving the way for future enhancements.

--- youtube/src/application/chat_service.rs
+++ youtube/src/application/chat_service.rs
@@ -1,19 +1,19 @@
 use crate::domain::{
-    repositories::{LiveChatRepository, SimpleChatRepository},
-    simple_chat::SimpleChatEntity,
+    live_chat::repository::FetchLiveChatRepository,
+    simple_chat::{repository::SaveSimpleChatRepository, SimpleChatEntity},
 };
 use anyhow::Context;
 use std::convert::TryInto;
 
 pub struct ChatConvertService {
-    from_chat: Box<dyn LiveChatRepository>,
-    to_chat: Box<dyn SimpleChatRepository>,
+    from_chat: Box<dyn FetchLiveChatRepository>,
+    to_chat: Box<dyn SaveSimpleChatRepository>,
 }
 
 impl ChatConvertService {
     pub fn new(
-        from_chat: Box<dyn LiveChatRepository>,
-        to_chat: Box<dyn SimpleChatRepository>,
+        from_chat: Box<dyn FetchLiveChatRepository>,
+        to_chat: Box<dyn SaveSimpleChatRepository>,
     ) -> Self {
         Self { from_chat, to_chat }
     }

1d423e487e140a1fa6b6740b6b3922d2b3744cd5
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-04 00:55:39 +0900

    Refactor ChatFileService to enhance chat conversion methods
    - Updated the `convert_with_path` and `convert_with_type` methods to utilize new repository building methods, improving error handling and clarity.
    - Changed the method calls to `convert_with_lines` for better alignment with the updated ChatConvertService functionality.
    - These changes contribute to a more organized and maintainable chat file service, aligning with ongoing efforts to enhance code clarity and functionality.

--- youtube/src/interface/chat_file_service.rs
+++ youtube/src/interface/chat_file_service.rs
@@ -15,22 +15,30 @@ impl<'a> ChatFileService<'a> {
         Self { base_path }
     }
 
-    pub fn convert_with_path(&self, path: &PathBuf) -> anyhow::Result<()> {
-        let live_chat_repository = Box::new(IoLiveChatRepository::from(self.base_path.clone()));
-        let simple_chat_repository = Box::new(IoSimpleChatRepository::from(path.clone()));
+    pub fn convert_with_path(&self, to_path: &PathBuf) -> anyhow::Result<()> {
+        let (_, live_chat_repository) = IoLiveChatRepository::build_opened_file(self.base_path)?;
+        let live_chat_repository = Box::new(live_chat_repository);
+
+        let (_, simple_chat_repository) = IoSimpleChatRepository::build_created_file(to_path)?;
+        let simple_chat_repository = Box::new(simple_chat_repository);
+
         let mut chat_convert_service =
             ChatConvertService::new(live_chat_repository, simple_chat_repository);
-        chat_convert_service.convert()
+        chat_convert_service.convert_with_lines()
     }
 
     pub fn convert_with_type(&self, file_type: &String) -> anyhow::Result<()> {
         let mut to_path = self.base_path.clone();
         to_path.set_extension(file_type);
 
-        let live_chat_repository = Box::new(IoLiveChatRepository::from(self.base_path.clone()));
-        let simple_chat_repository = Box::new(IoSimpleChatRepository::from(to_path.clone()));
+        let (_, live_chat_repository) = IoLiveChatRepository::build_opened_file(self.base_path)?;
+        let live_chat_repository = Box::new(live_chat_repository);
+
+        let (_, simple_chat_repository) = IoSimpleChatRepository::build_created_file(&to_path)?;
+        let simple_chat_repository = Box::new(simple_chat_repository);
+
         let mut chat_convert_service =
             ChatConvertService::new(live_chat_repository, simple_chat_repository);
-        chat_convert_service.convert()
+        chat_convert_service.convert_with_lines()
     }
 }

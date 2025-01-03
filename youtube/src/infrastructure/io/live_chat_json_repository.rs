0eb5101771f1fed551e014aff48b48de439eac00
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-04 00:53:45 +0900

    Refactor IoLiveChatRepository to consolidate file and in-memory handling
    - Removed the obsolete FileRepository and InMemoryRepository modules, integrating their functionality directly into IoLiveChatRepository.
    - Updated the IoLiveChatRepository struct to utilize Rc<Mutex<T>> for thread-safe access to both file and in-memory data sources.
    - Enhanced the all() and one_chunk() methods to read from the inner repository, improving error handling and code clarity.
    - These changes streamline the repository structure, enhancing modularity and maintainability in chat data handling.

--- youtube/src/infrastructure/io/live_chat_json_repository.rs
+++ youtube/src/infrastructure/io/live_chat_json_repository.rs
@@ -1,35 +1,78 @@
-pub(crate) mod file;
-pub(crate) mod in_memory;
-
 use crate::domain::live_chat::{repository::FetchLiveChatRepository, LiveChatEntity};
-use file::FileRepository;
-use in_memory::InMemoryRepository;
-use std::path::PathBuf;
+use anyhow::Context as _;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Cursor, Read},
+    path::PathBuf,
+    rc::Rc,
+    sync::Mutex,
+};
 
-pub struct IoLiveChatRepository<T> {
-    inner: T,
+pub(crate) struct IoLiveChatRepository<T> {
+    inner: Rc<Mutex<T>>,
 }
 
-impl From<PathBuf> for IoLiveChatRepository<FileRepository> {
-    fn from(value: PathBuf) -> Self {
-        let inner = FileRepository::from(value);
-        Self { inner }
+impl IoLiveChatRepository<File> {
+    pub fn build_opened_file(file_path: &PathBuf) -> anyhow::Result<(Rc<Mutex<File>>, Self)> {
+        let file = File::open(file_path).context("Failed to create file")?;
+        let file_mutex = Rc::new(Mutex::new(file));
+
+        let repository = Self {
+            inner: Rc::clone(&file_mutex),
+        };
+        Ok((file_mutex, repository))
     }
 }
 
-impl From<String> for IoLiveChatRepository<InMemoryRepository> {
-    fn from(value: String) -> Self {
-        let inner = InMemoryRepository::from(value);
-        Self { inner }
+impl<T> IoLiveChatRepository<Cursor<T>> {
+    pub fn build_in_memory(inner: T) -> anyhow::Result<(Rc<Mutex<Cursor<T>>>, Self)> {
+        let cursor = Cursor::new(inner);
+        let cursor_mutex = Rc::new(Mutex::new(cursor));
+
+        let repository = Self {
+            inner: Rc::clone(&cursor_mutex),
+        };
+        Ok((cursor_mutex, repository))
     }
 }
 
-impl<T: FetchLiveChatRepository> FetchLiveChatRepository for IoLiveChatRepository<T> {
+impl<R> FetchLiveChatRepository for IoLiveChatRepository<R>
+where
+    R: Read,
+{
     fn all(&self) -> anyhow::Result<Vec<LiveChatEntity>> {
-        self.inner.all()
+        let inner_mutex = Rc::clone(&self.inner);
+        let mut inner_lock = inner_mutex.lock().unwrap();
+        let inner = &mut *inner_lock;
+        let buffered = BufReader::new(inner);
+
+        let mut live_chats = Vec::new();
+
+        for (line_number, line) in buffered.lines().enumerate() {
+            let line_number = line_number + 1;
+            let line = line?;
+
+            let live_chat = serde_json::from_str::<LiveChatEntity>(&line)
+                .with_context(|| format!("Failed to convert at {line_number} row"))?;
+
+            live_chats.push(live_chat);
+        }
+
+        Ok(live_chats)
     }
 
     fn one_chunk(&self) -> anyhow::Result<LiveChatEntity> {
-        self.inner.one_chunk()
+        let inner_mutex = Rc::clone(&self.inner);
+        let mut inner_lock = inner_mutex.lock().unwrap();
+        let inner = &mut *inner_lock;
+        let mut buffered = BufReader::new(inner);
+
+        let mut content = String::new();
+        buffered.read_to_string(&mut content)?;
+
+        let live_chat = serde_json::from_str::<LiveChatEntity>(&content)
+            .with_context(|| format!("Failed to convert the content"))?;
+
+        Ok(live_chat)
     }
 }

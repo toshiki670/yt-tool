15f9182b35c0de771d6b66d379a2e0ba56ce9520
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-03 18:55:03 +0900

    Refactor IoLiveChatRepository to enhance modularity and introduce in-memory support
    - Reorganized the IoLiveChatRepository structure to support both file-based and in-memory data sources by introducing InMemoryRepository.
    - Simplified the implementation of the all() and one_chunk() methods to delegate functionality to the inner repository, improving code clarity and maintainability.
    - Updated the FileRepository to directly handle file operations, enhancing error handling and readability.
    - These changes contribute to a more modular and efficient chat repository system, aligning with ongoing efforts to improve code organization and maintainability.

--- youtube/src/infrastructure/io/live_chat_json_repository.rs
+++ youtube/src/infrastructure/io/live_chat_json_repository.rs
@@ -1,53 +1,35 @@
 pub(crate) mod file;
+pub(crate) mod in_memory;
 
 use crate::domain::live_chat::{repository::FetchLiveChatRepository, LiveChatEntity};
-use anyhow::Context as _;
-use std::io::{BufRead as _, BufReader, Read};
-use thiserror::Error;
+use file::FileRepository;
+use in_memory::InMemoryRepository;
+use std::path::PathBuf;
 
 pub struct IoLiveChatRepository<T> {
     inner: T,
 }
 
-#[derive(Error, Debug)]
-pub enum IoLiveChatRepositoryError {
-    #[error("Failed to convert at {0} row")]
-    FailedToConvertInLines(usize),
-}
-
-impl<R: Read> IoLiveChatRepository<R> {
-    pub fn open(inner: R) -> anyhow::Result<Self> {
-        Ok(Self { inner })
+impl From<PathBuf> for IoLiveChatRepository<FileRepository> {
+    fn from(value: PathBuf) -> Self {
+        let inner = FileRepository::from(value);
+        Self { inner }
     }
 }
 
-impl<R: Read> FetchLiveChatRepository for IoLiveChatRepository<R>
-where
-    for<'a> &'a R: Read,
-{
+impl From<String> for IoLiveChatRepository<InMemoryRepository> {
+    fn from(value: String) -> Self {
+        let inner = InMemoryRepository::from(value);
+        Self { inner }
+    }
+}
+
+impl<T: FetchLiveChatRepository> FetchLiveChatRepository for IoLiveChatRepository<T> {
     fn all(&self) -> anyhow::Result<Vec<LiveChatEntity>> {
-        let mut live_chats = Vec::new();
-
-        for (line_number, line) in BufReader::new(&self.inner).lines().enumerate() {
-            let line_number = line_number + 1;
-            let line = line?;
-
-            let live_chat = serde_json::from_str::<LiveChatEntity>(&line)
-                .with_context(|| IoLiveChatRepositoryError::FailedToConvertInLines(line_number))?;
-
-            live_chats.push(live_chat);
-        }
-
-        Ok(live_chats)
+        self.inner.all()
     }
 
     fn one_chunk(&self) -> anyhow::Result<LiveChatEntity> {
-        let mut content = String::new();
-        BufReader::new(&self.inner).read_to_string(&mut content)?;
-
-        let live_chat = serde_json::from_str::<LiveChatEntity>(&content)
-            .with_context(|| format!("Failed to convert the content"))?;
-
-        Ok(live_chat)
+        self.inner.one_chunk()
     }
 }
--- youtube/src/infrastructure/io/live_chat_json_repository/file.rs
+++ youtube/src/infrastructure/io/live_chat_json_repository/file.rs
@@ -1,46 +1,56 @@
-use super::{IoLiveChatRepository, IoLiveChatRepositoryError};
 use crate::domain::live_chat::{repository::FetchLiveChatRepository, LiveChatEntity};
 use anyhow::Context as _;
-use std::{fs::File, path::PathBuf};
+use std::{
+    fs::File,
+    io::{BufRead as _, BufReader, Read as _},
+    path::PathBuf,
+};
 
-pub(crate) struct FileLiveChatRepository {
-    inner: IoLiveChatRepository<File>,
+pub(crate) struct FileRepository {
     path: PathBuf,
 }
 
-impl FileLiveChatRepository {
-    pub fn open(path: &PathBuf) -> anyhow::Result<Self> {
-        let file =
-            File::open(path).with_context(|| format!("Failed to open {}", &path.display()))?;
-        let inner = IoLiveChatRepository::open(file)?;
-        Ok(Self {
-            inner,
-            path: path.clone(),
-        })
+impl From<PathBuf> for FileRepository {
+    fn from(value: PathBuf) -> Self {
+        Self { path: value }
     }
 }
 
-impl FetchLiveChatRepository for FileLiveChatRepository {
+impl FetchLiveChatRepository for FileRepository {
     fn all(&self) -> anyhow::Result<Vec<LiveChatEntity>> {
-        match self.inner.all() {
-            Ok(live_chats) => Ok(live_chats),
-            Err(e) => {
-                if let Some(IoLiveChatRepositoryError::FailedToConvertInLines(line_number)) =
-                    e.downcast_ref::<IoLiveChatRepositoryError>()
-                {
-                    anyhow::bail!(
-                        "Failed to convert at {}:{}",
-                        self.path.display(),
-                        line_number
-                    );
-                } else {
-                    Err(e)
-                }
-            }
+        let mut live_chats = Vec::new();
+
+        let file = File::open(&self.path).with_context(|| format!("Failed to open {}", &self.path.display()))?;
+        let buffered = BufReader::new(file);
+
+        for (line_number, line) in buffered.lines().enumerate() {
+            let line_number = line_number + 1;
+            let line = line?;
+
+            let live_chat = serde_json::from_str::<LiveChatEntity>(&line).with_context(|| {
+                format!(
+                    "Failed to convert at {}:{}",
+                    self.path.display(),
+                    line_number
+                )
+            })?;
+
+            live_chats.push(live_chat);
         }
+
+        Ok(live_chats)
     }
 
     fn one_chunk(&self) -> anyhow::Result<LiveChatEntity> {
-        self.inner.one_chunk()
+        let mut content = String::new();
+
+        let file = File::open(&self.path).with_context(|| format!("Failed to open {}", &self.path.display()))?;
+        let mut buf = BufReader::new(file);
+        buf.read_to_string(&mut content)?;
+
+        let live_chat = serde_json::from_str::<LiveChatEntity>(&content)
+            .with_context(|| format!("Failed to convert the content"))?;
+
+        Ok(live_chat)
     }
 }

58c8a02dc8e7b3ff85ee8de98a3e6bf249816e42
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-05 02:13:51 +0900

    Add FormattedJsonService for live chat JSON data processing
    - Introduced FormattedJsonService to manage and convert live chat JSON data into CSV format.
    - Updated the interface module to include the new service and modified lib.rs to expose it.
    - Added tests for the FormattedJsonService to validate the generation of CSV files from live chat JSON input.
    - These changes enhance the functionality of the chat services by providing a dedicated service for formatted JSON processing and ensuring accurate data transformation.

--- youtube/src/interface.rs
+++ youtube/src/interface.rs
@@ -3,4 +3,5 @@
 
 pub(crate) mod chat_file_service;
 pub(crate) mod chat_in_memory_service;
+pub(crate) mod formatted_json_service;
 pub(crate) mod live_chat_json_service;
--- youtube/src/lib.rs
+++ youtube/src/lib.rs
@@ -8,5 +8,6 @@ pub mod prelude {
 
     pub use crate::interface::chat_file_service::*;
     pub use crate::interface::chat_in_memory_service::*;
+    pub use crate::interface::formatted_json_service::*;
     pub use crate::interface::live_chat_json_service::*;
 }

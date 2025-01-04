494fe29eb7ea6886b900acfe31cd496f0840759f
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-04 17:36:16 +0900

    Refactor FetchLiveChatRepository method for clarity
    - Renamed the method `one_chunk` to `first` in the `FetchLiveChatRepository` trait to better reflect its functionality.
    - This change enhances code readability and aligns with recent refactoring efforts across the live chat components.

--- youtube/src/domain/live_chat/repository.rs
+++ youtube/src/domain/live_chat/repository.rs
@@ -2,5 +2,5 @@ use crate::domain::live_chat::LiveChatEntity;
 
 pub trait FetchLiveChatRepository {
     fn all(&self) -> anyhow::Result<Vec<LiveChatEntity>>;
-    fn one_chunk(&self) -> anyhow::Result<LiveChatEntity>;
+    fn first(&self) -> anyhow::Result<LiveChatEntity>;
 }

f96fb11099a53874af3b0dee99d6727ccc47c704
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-04 14:23:51 +0900

    Add documentation for prelude module in the library
    - Introduced a module-level documentation comment for the `prelude` module, clarifying its purpose and contents.
    - This enhancement improves code clarity and aligns with ongoing efforts to provide comprehensive documentation across the project.

--- youtube/src/lib.rs
+++ youtube/src/lib.rs
@@ -4,6 +4,8 @@ mod infrastructure;
 mod interface;
 
 pub mod prelude {
+    //! This module contains the prelude for the library.
+
     pub use crate::interface::chat_file_service::*;
     pub use crate::interface::chat_in_memory_service::*;
 }

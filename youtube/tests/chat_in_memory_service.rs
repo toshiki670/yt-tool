fbf73a4a0076eeb5f1ce27f657e0aa446f6ec18e
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-04 21:49:45 +0900

    Add tests for generating CSV data from live chat JSON in ChatInMemoryService
    - Introduced a new test module to validate the generation of CSV data from live chat JSON input.
    - Implemented helper functions to provide expected data and input for testing.
    - Enhanced test coverage for the in-memory chat service, ensuring accurate data transformation.
    
    These changes improve the reliability of the ChatInMemoryService by verifying its functionality through comprehensive tests.

--- youtube/tests/chat_in_memory_service.rs
+++ youtube/tests/chat_in_memory_service.rs
@@ -37,3 +37,79 @@ mod generate_from_lines {
         format!("{}\n", csvs.join("\n"))
     }
 }
+
+mod generate_from_chunk {
+    use pretty_assertions::assert_eq;
+    use youtube::prelude::ChatInMemoryService;
+
+    #[test]
+    fn it_generates_simple_chat_csv_data_from_live_chat_json_data_with_in_memory(
+    ) -> anyhow::Result<()> {
+        let expected = expected_csv_data_for_test();
+
+        let input_json_data = live_chat_json_data_for_test();
+        let actual = ChatInMemoryService::generate_from_chunk(input_json_data)?;
+
+        assert_eq!(expected, actual);
+
+        Ok(())
+    }
+
+    fn live_chat_json_data_for_test() -> String {
+        r#"
+            {
+            "replayChatItemAction": {
+                "actions": [
+                {
+                    "clickTrackingParams": "clickTrackingParams",
+                    "addChatItemAction": {
+                    "item": {
+                        "liveChatTextMessageRenderer": {
+                        "message": { "runs": [{ "text": "メッセージ" }] },
+                        "authorName": { "simpleText": "authorName" },
+                        "authorPhoto": {
+                            "thumbnails": [
+                            {
+                                "url": "https://yt4.ggpht.com/",
+                                "width": 32,
+                                "height": 32
+                            },
+                            { "url": "https://yt4.ggpht.com/", "width": 64, "height": 64 }
+                            ]
+                        },
+                        "contextMenuEndpoint": {
+                            "clickTrackingParams": "clickTrackingParams",
+                            "commandMetadata": {
+                            "webCommandMetadata": { "ignoreNavigation": true }
+                            },
+                            "liveChatItemContextMenuEndpoint": { "params": "params==" }
+                        },
+                        "id": "id",
+                        "timestampUsec": "1733370114906095",
+                        "authorExternalChannelId": "authorExternalChannelId",
+                        "contextMenuAccessibility": {
+                            "accessibilityData": { "label": "Chat actions" }
+                        },
+                        "trackingParams": "trackingParams"
+                        }
+                    },
+                    "clientId": "clientId"
+                    }
+                }
+                ]
+            },
+            "videoOffsetTimeMsec": "-416809",
+            "isLive": true
+            }
+            "#.to_string()
+    }
+
+    fn expected_csv_data_for_test() -> String {
+        let mut csvs = Vec::new();
+
+        csvs.push("id,authorExternalChannelId,postedAt,authorName,content,isModerator,membershipMonths,category");
+        csvs.push("id,authorExternalChannelId,2024-12-05T12:41:54.906095+09:00,authorName,メッセージ,false,,ChatTextMessage");
+
+        format!("{}\n", csvs.join("\n"))
+    }
+}

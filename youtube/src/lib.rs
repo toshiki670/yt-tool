d68898aaad0fb22f0901a9fdce26f412e8972f66
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-05 02:15:57 +0900

    Remove ChatFileService and ChatInMemoryService from the interface module
    - Deleted the ChatFileService and ChatInMemoryService implementations, which were responsible for managing live chat JSON data both from files and in memory.
    - Updated the interface module and lib.rs to remove references to these services, streamlining the codebase.
    - This change simplifies the interface layer by focusing on the remaining services, enhancing maintainability and clarity.

--- youtube/src/lib.rs
+++ youtube/src/lib.rs
@@ -6,8 +6,6 @@ mod interface;
 pub mod prelude {
     //! This module contains the prelude for the library.
 
-    pub use crate::interface::chat_file_service::*;
-    pub use crate::interface::chat_in_memory_service::*;
     pub use crate::interface::formatted_json_service::*;
     pub use crate::interface::live_chat_json_service::*;
 }
--- youtube/tests/lib.rs
+++ youtube/tests/lib.rs
@@ -1,4 +1,2 @@
-mod chat_file_service;
-mod chat_in_memory_service;
 mod formatted_json_service;
 mod live_chat_json_service;

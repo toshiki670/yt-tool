f14718d76d3fd1ba9345a1eabbad32acacee6d30
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-04 22:27:08 +0900

    Add LiveChatMembershipItemRenderer to Item enum in live chat module
    - Introduced LiveChatMembershipItemRenderer variant to the Item enum, enhancing the representation of live chat items.
    - Added import for LiveChatTickerPaidStickerItemRenderer to support new rendering capabilities.
    - These changes improve the extensibility of the live chat module by accommodating additional item types.

--- youtube/src/domain/live_chat/item.rs
+++ youtube/src/domain/live_chat/item.rs
@@ -16,6 +16,7 @@ use serde::{Deserialize, Serialize};
 pub enum Item {
     #[default]
     None,
+    LiveChatMembershipItemRenderer(LiveChatMembershipItemRenderer),
     LiveChatPaidMessageRenderer(LiveChatPaidMessageRenderer),
     LiveChatPaidStickerRenderer(serde_json::Value),
     LiveChatSponsorshipsGiftPurchaseAnnouncementRenderer(
@@ -26,5 +27,4 @@ pub enum Item {
     LiveChatTickerPaidMessageItemRenderer(LiveChatTickerPaidMessageItemRenderer),
     LiveChatTickerSponsorItemRenderer(serde_json::Value),
     LiveChatViewerEngagementMessageRenderer(LiveChatViewerEngagementMessageRenderer),
-    LiveChatMembershipItemRenderer(LiveChatMembershipItemRenderer),
 }

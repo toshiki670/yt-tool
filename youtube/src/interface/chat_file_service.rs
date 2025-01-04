16cc146b186d4a2f1943a936a0b22c95b1f89aa5
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-04 12:57:42 +0900

    Enhance ChatFileService with new conversion methods and documentation
    - Added `convert_with_type` method to allow conversion of live chat JSON data to CSV based on file type, improving flexibility in data handling.
    - Enhanced documentation for `ChatFileService` and its methods, providing clearer guidance on usage and expected arguments/returns.
    - These changes contribute to a more robust and user-friendly chat file service, aligning with ongoing efforts to improve code clarity and functionality.

--- youtube/src/interface/chat_file_service.rs
+++ youtube/src/interface/chat_file_service.rs
@@ -6,15 +6,30 @@ use crate::infrastructure::io::{
 
 use std::path::PathBuf;
 
+/// This service provides an interface for managing and retrieving live chat JSON data from files.
 pub struct ChatFileService<'a> {
     base_path: &'a PathBuf,
 }
 
 impl<'a> ChatFileService<'a> {
+    /// Create a new ChatFileService instance.
+    ///
+    /// # Arguments
+    /// - `base_path`: The base path.
+    ///
+    /// # Returns
+    /// - `Self`: ChatFileService instance.
     pub fn new(base_path: &'a PathBuf) -> Self {
         Self { base_path }
     }
 
+    /// Convert live chat JSON data to simple chat CSV data.
+    ///
+    /// # Arguments
+    /// - `to_path`: The path to save the converted data.
+    ///
+    /// # Returns
+    /// - `anyhow::Result<()>`: Result of the conversion.
     pub fn convert_with_path(&self, to_path: &PathBuf) -> anyhow::Result<()> {
         let (_, live_chat_repository) = IoLiveChatRepository::build_opened_file(self.base_path)?;
         let live_chat_repository = Box::new(live_chat_repository);
@@ -27,6 +42,13 @@ impl<'a> ChatFileService<'a> {
         chat_convert_service.convert_with_lines()
     }
 
+    /// Convert live chat JSON data to simple chat CSV data.
+    ///
+    /// # Arguments
+    /// - `file_type`: The file type to save the converted data.
+    ///
+    /// # Returns
+    /// - `anyhow::Result<()>`: Result of the conversion.
     pub fn convert_with_type(&self, file_type: &String) -> anyhow::Result<()> {
         let mut to_path = self.base_path.clone();
         to_path.set_extension(file_type);

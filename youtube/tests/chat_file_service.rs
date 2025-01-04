4ec70c156fd2678e7365862147af8441dc1cc470
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-04 16:54:08 +0900

    Refactor chat_file_service tests to simplify CSV data generation
    - Removed unnecessary variable assignments in the `expected_csv_data_for_test` function for cleaner code.
    - Streamlined the generation of CSV data by directly formatting the joined strings, enhancing readability and maintainability of the test code.

--- youtube/tests/chat_file_service.rs
+++ youtube/tests/chat_file_service.rs
@@ -28,7 +28,6 @@ fn it_generates_simple_chat_csv_data_from_live_chat_json_data_with_path() -> any
 fn live_chat_json_data_for_test() -> String {
     let mut jsons = Vec::new();
 
-    // live_chat_text_message_renderer
     jsons.push(
         r#"{"replayChatItemAction":{"actions":[{"clickTrackingParams":"clickTrackingParams","addChatItemAction":{"item":{"liveChatTextMessageRenderer":{"message":{"runs":[{"text":"メッセージ"}]},"authorName":{"simpleText":"authorName"},"authorPhoto":{"thumbnails":[{"url":"https://yt4.ggpht.com/","width":32,"height":32},{"url":"https://yt4.ggpht.com/","width":64,"height":64}]},"contextMenuEndpoint":{"clickTrackingParams":"clickTrackingParams","commandMetadata":{"webCommandMetadata":{"ignoreNavigation":true}},"liveChatItemContextMenuEndpoint":{"params":"params=="}},"id":"id","timestampUsec":"1733370114906095","authorExternalChannelId":"authorExternalChannelId","contextMenuAccessibility":{"accessibilityData":{"label":"Chat actions"}},"trackingParams":"trackingParams"}},"clientId":"clientId"}}]},"videoOffsetTimeMsec":"-416809","isLive":true}"#
     );
@@ -42,8 +41,5 @@ fn expected_csv_data_for_test() -> String {
     csvs.push("id,authorExternalChannelId,postedAt,authorName,content,isModerator,membershipMonths,category");
     csvs.push("id,authorExternalChannelId,2024-12-05T12:41:54.906095+09:00,authorName,メッセージ,false,,ChatTextMessage");
 
-    let joined_with_newline = csvs.join("\n");
-    let append_newline_last = format!("{}\n", joined_with_newline);
-
-    append_newline_last
+    format!("{}\n", csvs.join("\n"))
 }

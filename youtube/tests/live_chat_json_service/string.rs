e03510f5a13ac0bd48981a4594fafcfcd490862a
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-05 17:09:27 +0900

    Add string module and corresponding test for LiveChatJsonService

---
+++ youtube/tests/live_chat_json_service/string.rs
@@ -0,0 +1,38 @@
+extern crate youtube;
+
+use pretty_assertions::assert_eq;
+use tempfile::tempdir;
+use youtube::prelude::LiveChatJsonService;
+
+#[test]
+fn it_generate_with_path() -> anyhow::Result<()> {
+    let temp_dir = tempdir()?;
+
+    let input_json = test_formatted_json_data();
+    let output_path = temp_dir.path().join("output.csv");
+
+    let chat_file_service = LiveChatJsonService::new(&input_json);
+    chat_file_service.generate_file_with_path(&output_path)?;
+
+    let expected = expected_csv_data_for_test();
+    let actual = std::fs::read_to_string(&output_path)?;
+    assert_eq!(expected, actual);
+
+    temp_dir.close()?;
+    Ok(())
+}
+
+fn expected_csv_data_for_test() -> String {
+    let mut csvs = Vec::new();
+
+    csvs.push("id,authorExternalChannelId,postedAt,authorName,content,isModerator,membershipMonths,category");
+    csvs.push("id,authorExternalChannelId,2024-12-05T12:41:54.906095+09:00,authorName,メッセージ,false,,ChatTextMessage");
+
+    format!("{}\n", csvs.join("\n"))
+}
+
+fn test_formatted_json_data() -> String {
+    let s = r#"{"replayChatItemAction":{"actions":[{"clickTrackingParams":"clickTrackingParams","addChatItemAction":{"item":{"liveChatTextMessageRenderer":{"message":{"runs":[{"text":"メッセージ"}]},"authorName":{"simpleText":"authorName"},"authorPhoto":{"thumbnails":[{"url":"https://yt4.ggpht.com/","width":32,"height":32},{"url":"https://yt4.ggpht.com/","width":64,"height":64}]},"contextMenuEndpoint":{"clickTrackingParams":"clickTrackingParams","commandMetadata":{"webCommandMetadata":{"ignoreNavigation":true}},"liveChatItemContextMenuEndpoint":{"params":"params=="}},"id":"id","timestampUsec":"1733370114906095","authorExternalChannelId":"authorExternalChannelId","contextMenuAccessibility":{"accessibilityData":{"label":"Chat actions"}},"trackingParams":"trackingParams"}},"clientId":"clientId"}}]},"videoOffsetTimeMsec":"-416809","isLive":true}"#;
+
+    s.to_string()
+}

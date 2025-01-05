7ae802c33a36de9d99df52f5823858d01d572d17
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-05 16:17:49 +0900

    Refactor and reorganize tests for FormattedJsonService and LiveChatJsonService
    - Deleted outdated test files for FormattedJsonService and LiveChatJsonService.
    - Introduced new test structure with dedicated modules and JSON test files for improved organization.
    - Updated test paths to utilize new JSON files: base.json for FormattedJsonService and live_chat.json for LiveChatJsonService.
    - Enhanced test clarity and maintainability by centralizing file path management and ensuring correct input data format.

---
+++ youtube/tests/live_chat_json_service/file.rs
@@ -0,0 +1,65 @@
+extern crate youtube;
+
+use pretty_assertions::assert_eq;
+use std::{env, path::PathBuf};
+use tempfile::tempdir;
+use youtube::prelude::LiveChatJsonService;
+
+fn test_json_dir() -> PathBuf {
+    env::current_dir()
+        .unwrap()
+        .join("tests/live_chat_json_service/json/")
+}
+
+#[test]
+fn it_generate_with_path() -> anyhow::Result<()> {
+    let temp_dir = tempdir()?;
+
+    let input_path = test_json_dir().join("live_chat.json");
+    let output_path = temp_dir.path().join("output.csv");
+
+    let chat_file_service = LiveChatJsonService::new(&input_path);
+    chat_file_service.generate_file_with_path(&output_path)?;
+
+    let expected = expected_csv_data_for_test();
+    let actual = std::fs::read_to_string(&output_path)?;
+    assert_eq!(expected, actual);
+
+    temp_dir.close()?;
+    Ok(())
+}
+
+#[test]
+fn it_generate_with_type() -> anyhow::Result<()> {
+    let temp_dir = tempdir()?;
+
+    let file_name = "live_chat.json";
+    let base_input_path = test_json_dir().join(file_name);
+
+    let input_path = temp_dir.path().join(file_name);
+    std::fs::copy(&base_input_path, &input_path)?;
+
+    let output_type = "csv".to_string();
+
+    let chat_file_service = LiveChatJsonService::new(&input_path);
+    chat_file_service.generate_file_with_type(&output_type)?;
+
+    let mut to_path = input_path.clone();
+    to_path.set_extension(output_type);
+
+    let expected = expected_csv_data_for_test();
+    let actual = std::fs::read_to_string(&to_path)?;
+    assert_eq!(expected, actual);
+
+    temp_dir.close()?;
+    Ok(())
+}
+
+fn expected_csv_data_for_test() -> String {
+    let mut csvs = Vec::new();
+
+    csvs.push("id,authorExternalChannelId,postedAt,authorName,content,isModerator,membershipMonths,category");
+    csvs.push("id,authorExternalChannelId,2024-12-05T12:41:54.906095+09:00,authorName,メッセージ,false,,ChatTextMessage");
+
+    format!("{}\n", csvs.join("\n"))
+}

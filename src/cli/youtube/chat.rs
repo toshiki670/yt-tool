069b9afaae25cc256b176e5c5f57468f8cbfbe0e
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-06 19:55:00 +0900

    Refactor chat command arguments to support glob patterns and enhance file handling
    - Changed `input_file` to `input_patterns` to accept multiple glob patterns for input files.
    - Added `rename_with_timestamp` option to allow renaming output files with timestamps.
    - Updated `output_type` to have a default value of "csv".
    - Implemented `expend_glob_input_patterns` function to expand glob patterns and handle input files.
    - Adjusted the `route` method to utilize the new input handling logic.

--- src/cli/youtube/chat.rs
+++ src/cli/youtube/chat.rs
@@ -3,24 +3,30 @@
 
 use crate::cli::Route;
 use clap::{ArgGroup, ValueEnum, ValueHint};
+use glob::glob;
 use std::{fmt::Display, path::PathBuf};
 use youtube::prelude::*;
 
 #[derive(clap::Args, Debug)]
 #[command(name = "Comment File Feature")]
-#[clap(group(ArgGroup::new("output")
-    .args(&["output_file"])
-    .conflicts_with("output_type"))
-)]
 pub(super) struct Args {
-    #[clap(value_name = "INPUT FILE", value_hint = ValueHint::FilePath)]
-    input_file: PathBuf,
+    #[clap(
+        value_name = "INPUT PATTERNS",
+        help = "Input files (glob patterns supported: *.json)"
+    )]
+    input_patterns: Vec<String>,
 
-    #[clap(short = 'o', long, value_name = "OUTPUT FILE", value_hint = ValueHint::FilePath)]
-    output_file: Option<PathBuf>,
+    #[clap(short = 'r', long, help = "Rename to include the timestamp")]
+    rename_with_timestamp: bool,
 
-    #[arg(short = 't', long, value_name = "FILE_TYPE", help = "Output file type")]
-    output_type: Option<FileType>,
+    #[arg(
+        short = 't',
+        long,
+        default_value = "csv",
+        value_name = "FILE_TYPE",
+        help = "Output file type"
+    )]
+    output_type: FileType,
 }
 
 #[derive(Clone, Debug, ValueEnum)]
@@ -38,17 +44,35 @@ impl Display for FileType {
 
 impl Route for Args {
     fn route(&self) -> anyhow::Result<()> {
-        let input_file = &self.input_file;
-        let chat_file_service = LiveChatJsonService::new(input_file);
+        // Expand glob patterns and create a list of input files
+        let input_files = expend_glob_input_patterns(&self.input_patterns)?;
 
-        if let Some(output_file) = &self.output_file {
-            chat_file_service.generate_file_with_path(output_file)?;
-        } else if let Some(output_type) = &self.output_type {
-            chat_file_service.generate_file_with_type(&output_type.to_string())?;
-        } else {
-            unreachable!();
+        let chat_file_service = LiveChatJsonService::new(&input_files);
+
+        match &self.output_type {
+            FileType::Csv => {
+                chat_file_service.generate_file_with_type(&output_type.to_string())?;
+            }
         }
 
         Ok(())
     }
+}
+
+fn expend_glob_input_patterns(input_patterns: &Vec<String>) -> anyhow::Result<Vec<PathBuf>> {
+    let mut input_files = Vec::new();
+    for pattern in input_patterns {
+        let paths = glob(pattern).map_err(|e| anyhow!("Glob pattern is invalid: {}", e))?;
+
+        for entry in paths {
+            match entry {
+                Ok(path) => input_files.push(path),
+                Err(e) => bail!("Warning: Failed to read path: {}", e),
+            }
+        }
+    }
+    if input_files.is_empty() {
+        anyhow::bail!("No input files were found");
+    }
+    Ok(input_files)
 }

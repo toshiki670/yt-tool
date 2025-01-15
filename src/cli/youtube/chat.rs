89b71b272a706f186e455f4be6a9694bbfa046da
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-16 02:12:06 +0900

    Refactor Args struct in chat.rs to simplify output handling
    - Removed the output_type field and associated FileType enum to streamline the argument parsing process.
    - Consolidated file generation logic to always produce CSV files, regardless of the rename_with_timestamp flag.
    - Updated imports to utilize the new expend_glob_input_patterns function from the utils module for improved file handling.
    
    These changes enhance the clarity and efficiency of the chat service's command-line interface.

--- src/cli/youtube/chat.rs
+++ src/cli/youtube/chat.rs
@@ -2,10 +2,7 @@
 // https://tech.natsuneko.blog/entry/2022/03/15/exclusive-command-options-in-clap
 
 use crate::cli::Route;
-use anyhow::{anyhow, bail};
-use clap::ValueEnum;
-use glob::glob;
-use std::{fmt::Display, path::PathBuf};
+use crate::utils::file_utils::expend_glob_input_patterns;
 use youtube::prelude::*;
 
 #[derive(clap::Args, Debug)]
@@ -19,28 +16,6 @@ pub(super) struct Args {
 
     #[clap(short = 'r', long, help = "Rename to include the timestamp")]
     rename_with_timestamp: bool,
-
-    #[arg(
-        short = 't',
-        long,
-        default_value = "csv",
-        value_name = "FILE_TYPE",
-        help = "Output file type"
-    )]
-    output_type: FileType,
-}
-
-#[derive(Clone, Debug, ValueEnum)]
-enum FileType {
-    Csv,
-}
-
-impl Display for FileType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            FileType::Csv => write!(f, "csv"),
-        }
-    }
 }
 
 impl Route for Args {
@@ -50,30 +25,12 @@ impl Route for Args {
 
         let chat_file_service = LiveChatJsonService::new(&input_files);
 
-        match &self.output_type {
-            FileType::Csv => {
-                chat_file_service.generate_file_with_type(&output_type.to_string())?;
-            }
+        if self.rename_with_timestamp {
+            chat_file_service.generate_files_with_csv()?;
+        } else {
+            chat_file_service.generate_files_with_csv()?;
         }
 
         Ok(())
     }
-}
-
-fn expend_glob_input_patterns(input_patterns: &Vec<String>) -> anyhow::Result<Vec<PathBuf>> {
-    let mut input_files = Vec::new();
-    for pattern in input_patterns {
-        let paths = glob(pattern).map_err(|e| anyhow!("Glob pattern is invalid: {}", e))?;
-
-        for entry in paths {
-            match entry {
-                Ok(path) => input_files.push(path),
-                Err(e) => bail!("Warning: Failed to read path: {}", e),
-            }
-        }
-    }
-    if input_files.is_empty() {
-        bail!("No input files were found");
-    }
-    Ok(input_files)
 }

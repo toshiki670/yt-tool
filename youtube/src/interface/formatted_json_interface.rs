b0ced788f50d0d935b56d088f8fc57c01d51bb77
Toshiki <bushy.trivial.0o@icloud.com>
2025-01-27 21:05:16 +0900

    Refactor FormattedJsonInterface path handling
    - Update `generate_file_with_path` to accept generic `&Path`
    - Use `to_path_buf()` for converting Path to PathBuf
    - Simplify Path import in the interface

--- youtube/src/interface/formatted_json_interface.rs
+++ youtube/src/interface/formatted_json_interface.rs
@@ -1,6 +1,6 @@
 use crate::application::chat_service::ChatConvertService;
 use crate::infrastructure::io::chat_service_repository::IoChatServiceRepository;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 /// This service provides an interface for managing and retrieving live chat JSON data from files.
@@ -24,9 +24,9 @@ impl FormattedJsonInterface<'_, PathBuf> {
     ///
     /// # Arguments
     /// - `to_path`: The path to save the converted data.
-    pub async fn generate_file_with_path(&self, to_path: &PathBuf) -> anyhow::Result<()> {
+    pub async fn generate_file_with_path(&self, to_path: &Path) -> anyhow::Result<()> {
         let from_path = self.inner.clone();
-        let to_path = to_path.clone();
+        let to_path = to_path.to_path_buf();
 
         let repositories = vec![Arc::new(IoChatServiceRepository::file_to_file(
             from_path, to_path,
